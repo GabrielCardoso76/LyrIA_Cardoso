4ea3a018175f6e76072d682cd2320d56e1e943e2
GabrielCardoso76 <torresgbriel8@gmail.com>
2025-10-20 14:57:13 -0300

    codigo atual 2

--- lyria-web/src/services/api.js
+++ lyria-web/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const baseURL = import.meta.env.VITE_API_BASE_URL;
+//export const baseURL = import.meta.env.VITE_API_BASE_URL;
+export const baseURL = "https://lyria-back.onrender.com";
+console.log(baseURL);
 
 if (!baseURL) {
   console.error(
@@ -10,7 +12,7 @@ if (!baseURL) {
 
 const api = axios.create({
   baseURL: baseURL,
-
+  withCredentials: true, // garante envio de cookies
   headers: {
     "Content-Type": "application/json",
   },
